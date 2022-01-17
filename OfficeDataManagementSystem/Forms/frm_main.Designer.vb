d44af8134c41531da416ae991af4d7d6f0effb8c
SaroeunSaing <saingsaroeuninfo@gmail.com>
2022-01-17 14:41:02 +0700

    hi

--- OfficeDataManagementSystem/Forms/frm_main.Designer.vb
+++ OfficeDataManagementSystem/Forms/frm_main.Designer.vb
@@ -596,7 +596,7 @@ Partial Class FormMain
         Me.Label1.BackColor = System.Drawing.Color.Transparent
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label1.ForeColor = System.Drawing.Color.MidnightBlue
-        Me.Label1.Location = New System.Drawing.Point(87, 33)
+        Me.Label1.Location = New System.Drawing.Point(156, 1)
         Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(401, 26)
